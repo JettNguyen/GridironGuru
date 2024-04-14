09f516b0d900972e6860228b78823b55c636ed4f
JettNguyen <jett.nguyen@ufl.edu>
2024-04-14 15:38:40 -0400

    updated user interface for the results a bit. All that needs to be done is implementing the other data structure

---
+++ src/Play.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+//represents a play
+struct Play {
+    int gameID;
+    string gameDate;
+    int quarter;
+    int minutes;
+    int seconds;
+    int timeAsInt;
+    string offense;
+    string defense;
+    int down;
+    int toGo;
+    int yardLine;
+    bool resultIsFirstDown;
+    float firstDownWeight;
+    string description;
+    int resultingYards;
+    float yardsWeight;
+    string formation;
+    string playType;
+    bool isRush;
+    bool isPass;
+    bool isIncomplete;
+    bool isTouchdown;
+    float touchdownWeight;
+    string passType;
+    bool isSack;
+    bool isInterception;
+    float interceptionWeight;
+    bool isFumble;
+    float fumbleWeight;
+    bool isTwoPointConversion;
+    bool isTwoPointConversionSuccessful;
+    float twoPointWeight;
+    string rushDirection;
+
+public:
+    Play();
+};
